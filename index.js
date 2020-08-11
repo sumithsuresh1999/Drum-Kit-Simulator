d999348f7556e6fface6139f234a7ff19ae710c5
Sumith G S <65539871+sumithsuresh1999@users.noreply.github.com>
2020-08-11 15:32:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+//All Funtions
+
+// Sounds
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kickbass = new Audio("sounds/kick-bass.mp3");
+      kickbass.play();
+      break;
+
+    default: console.log(buttonInnerHTML);
+  }
+}
+
+// Animations
+function buttonAnimation(activeKey) {
+  var activeButton = document.querySelector("." + activeKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+
+// Main Code
+
+// Detecting Mouse Clicks
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// Detecting Keyboard Clicks
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
